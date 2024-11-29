fc453d4b647bff96b5be89491c0789b6c33df545
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-11-29 23:16:17 +0000

    Comments movies.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,13 +1,15 @@
 import MovieItem from "./movieitem";
 
+//Movies functional componenet to render movie list
 function Movies(props) {
     return (
         <>
+        {/*Map over myMovies array as a prop and render a MovieItem for each movie*/}
             {props.myMovies.map((movie) => (
                 <MovieItem
-                    mymovie={movie}
-                    key={movie._id}
-                    Reload={props.ReloadData}
+                    mymovie={movie} //pass current movie object as a prop
+                    key={movie._id} //use the movie's unique ID as key for efficient rendering
+                    Reload={props.ReloadData} //Pass reloadData function ro refresh movie data
                 />
             ))}
         </>
