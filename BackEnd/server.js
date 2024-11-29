d9a63cf215e3085ae533be64276ee019a473aa74
Rebecca Nolan <G00423921@atu.ie>
2024-11-29 11:36:45 +0000

    Step 2 complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -72,6 +72,15 @@ app.post('/api/movies',async (req, res)=>{
     res.status(201).json({"message":"Movie Added!",Movie:newMovie}); //respond with a success message and added movie
 })
 
+//deletes movie
+app.delete('/api/movie/:id', async (req, res) => {
+  
+  console.log('Deleting movie with ID:', req.params.id);
+  const movie = await movieModel.findByIdAndDelete(req.params.id);
+  res.status(200).send({ message: "Movie deleted successfully", movie });
+  
+})
+
 //start express server on specified port
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`); //Log server URL
--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -2,14 +2,27 @@ import { useEffect } from "react"; //hook for side effects
 import Card from 'react-bootstrap/Card'; //bootstrap componenet for styling
 //import link 
 import { Link } from "react-router-dom"; //react router componenet for navigation
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
 
 //MovieItem functional component
 const MovieItem = (props)=> {
+
   //useEffect to log movie data when mymovie prop changes
   useEffect(() => {
     console.log("Movie Item:", props.mymovie); //log movie for debugging
   }, [props.mymovie]); // Only run this effect when the mymovie prop changes
 
+  const handleDelete = (e) => {
+    e.preventDefault();
+    axios.delete('http://localhost:4000/api/movie/' + props.mymovie._id)
+        .then(() => {
+            props.Reload(); // Refresh the movie list after deletion
+        })
+        .catch((error) => {
+            console.error("Error deleting movie:", error);
+        });
+  };
   //form to display movie
   return (
     <div>
@@ -21,11 +34,12 @@ const MovieItem = (props)=> {
             <footer>{props.mymovie.year}</footer>
           </blockquote>
         </Card.Body>
-        {/*Add comment here*/}
         <Link to={"/edit/" + props.mymovie._id} className="btn btn-primary">Edit</Link>
       </Card>
+      <Button variant="danger" onClick={handleDelete}>Delete</Button>
     </div>
   );
 }
+
 
 export default MovieItem;
